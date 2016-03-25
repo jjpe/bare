309a9ceec72f9e0f9228d3e6e2fe7f88a0a9c482
Joey Ezechiëls <joey.ezechiels@gmail.com>
2016-03-25 18:23:00 +0100

    Introduce unit test for basic bare functionality

--- src/bare/mod.rs
+++ src/bare/mod.rs
@@ -40,3 +40,75 @@ pub fn propose_renames(paths: &[&Path],
     }
     RenameProposal { renames: renames, not_found: not_found }
 }
+
+
+
+
+#[cfg(test)]
+mod tests {
+    use bare;
+    use regex::Regex;
+    use std::fs;
+    use std::fs::File;
+    use std::path::{Path, PathBuf};
+
+    #[test]
+    fn propose_renames_basic() {
+        let path_strings = file_path_strings();
+        let paths: Vec<_> = path_strings.iter()
+            .map(|s| Path::new(s))
+            .collect();
+        let patterns = vec![
+            (regex(r"(-)bar"),        replacement("_coconut")),
+            (regex(r"\(grault\)"),    replacement("grault")),
+            (regex(r"_"),             replacement(".")),
+        ];
+        ensure_exist(&paths);
+        let proposal = bare::propose_renames(&paths, &patterns);
+        assert_eq!(proposal.not_found, vec![] as Vec<String>);
+        assert_eq!(proposal.renames, vec![
+            (PathBuf::from("/tmp/bare_test/foo.bar"),   // Old path, and ...
+             PathBuf::from("/tmp/bare_test/foo.bar")),  // ...proposed new path
+            (PathBuf::from("/tmp/bare_test/foo-bar.qux"),
+             PathBuf::from("/tmp/bare_test/foo.coconut.qux")),
+            (PathBuf::from("/tmp/bare_test/corge_(grault).quux"),
+             PathBuf::from("/tmp/bare_test/corge.grault.quux"))
+        ]);
+    }
+
+    #[cfg(unix)]
+    fn file_path_strings<'a>() -> Vec<&'a str> {
+        vec![
+            "/tmp/bare_test/foo.bar",
+            "/tmp/bare_test/foo-bar.qux",
+            "/tmp/bare_test/corge_(grault).quux",
+        ]
+    }
+
+    #[cfg(windows)]
+    fn file_path_strings<'a>() -> Vec<&'a str> {
+        vec![
+            // TODO:
+        ]
+    }
+
+    fn regex(literal: &'static str) -> Regex {
+        Regex::new(literal).unwrap()
+    }
+
+    fn replacement(literal: &'static str) -> String {
+        String::from(literal)
+    }
+
+    fn ensure_exist(paths: &[&Path]) {
+        for path in paths {
+            if path.exists() {  continue;  }
+            if let Some(parent) = path.parent() {
+                if !parent.exists() {
+                    fs::create_dir_all(parent).unwrap();
+                }
+            }
+            File::create(path).unwrap();
+        }
+    }
+}
