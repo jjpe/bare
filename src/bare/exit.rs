26f653e645b163f1468c0283e4cf229a57cc9d27
Joey Ezechiëls <joey.ezechiels@gmail.com>
2016-03-29 11:49:45 +0200

    Rewrite the exit module

--- src/bare/exit.rs
+++ src/bare/exit.rs
@@ -1,21 +1,54 @@
 //! Exiting the program made trivial.
 
+use std::io;
+use std::io::Write;
 use std::process;
 
 /// Exit codes for the program.
+#[derive(Debug, Clone)]
 pub enum ExitCode {
-    Ok =                   0,
-    NotEnoughPatterns =    1,
-    MalformedPattern =     2,
-    NotEnoughFiles =       3,
+    Ok,
+    MalformedPattern(String),
+    MalformedRegex(String),
+    MissingRequiredCliArgument(String),
+    NotEnoughFiles,
+    NotEnoughPatterns(String),
+}
+
+fn exit(exit_code : ExitCode) {
+    io::stdout().flush().unwrap();
+    process::exit(match exit_code {
+        ExitCode::Ok => 0,
+        ExitCode::MalformedPattern(ref patterns) => {
+            println!("malformed pattern(s): {}", patterns);
+            1
+        },
+        ExitCode::MalformedRegex(ref patterns) => {
+            println!("malformed regex: {}", patterns);
+            2
+        },
+        ExitCode::MissingRequiredCliArgument(ref patterns) => {
+            println!("Need to provide one of {}", patterns);
+            3
+        },
+        ExitCode::NotEnoughFiles => {
+            println!("provide at least 1 file");
+            4
+        },
+        ExitCode::NotEnoughPatterns(ref patterns) => {
+            println!("not enough pattern(s) in {}", patterns);
+            5
+        },
+    });
 }
 
 /// Abnormally exit the program. The `exit_code` value specifies the reason.
 pub fn abort(exit_code : ExitCode) {
-    process::exit(exit_code as i32);
+    print!("Aborting, ");
+    exit(exit_code);
 }
 
 /// Normally exit the program.
 pub fn quit() {
-    process::exit(ExitCode::Ok as i32);
+    exit(ExitCode::Ok);
 }
