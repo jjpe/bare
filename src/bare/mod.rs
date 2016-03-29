ef69250c985b44e581e47f6a3d9f325c61f0a5ca
Joey Ezechiëls <joey.ezechiels@gmail.com>
2016-03-29 13:22:22 +0200

    Better rename tests

--- src/bare/mod.rs
+++ src/bare/mod.rs
@@ -53,47 +53,51 @@ pub fn propose_renames(paths: &[&Path], patterns: &[Pattern])
 mod tests {
     use bare;
     use regex::Regex;
+    use std::collections::HashMap;
     use std::fs;
     use std::fs::File;
     use std::path::{Path, PathBuf};
 
     #[test]
     fn propose_renames_basic() {
-        let path_strings = file_path_strings();
-        let paths: Vec<_> = path_strings.iter()
-            .map(|s| Path::new(s))
-            .collect();
-        let patterns = vec![
-            (regex(r"(-)bar"),        replacement("_coconut")),
-            (regex(r"\(grault\)"),    replacement("grault")),
-            (regex(r"_"),             replacement(".")),
-        ];
+        let (paths, patterns) = (paths(), patterns());
         ensure_exist(&paths);
-        let proposal = bare::propose_renames(&paths, &patterns);
-        assert_eq!(proposal.not_found, vec![] as Vec<String>);
-        assert_eq!(proposal.renames, vec![
-            (PathBuf::from("/tmp/bare_test/foo.bar"),   // Old path, and ...
-             PathBuf::from("/tmp/bare_test/foo.bar")),  // ...proposed new path
-            (PathBuf::from("/tmp/bare_test/foo-bar.qux"),
-             PathBuf::from("/tmp/bare_test/foo.coconut.qux")),
-            (PathBuf::from("/tmp/bare_test/corge_(grault).quux"),
-             PathBuf::from("/tmp/bare_test/corge.grault.quux"))
-        ]);
+        let (proposal, files_not_found) =
+            bare::propose_renames(&paths, &patterns);
+        assert_eq!(files_not_found, vec![] as Vec<PathBuf>);
+        assert_eq!(proposal, {
+            let mut m = HashMap::new();
+            m.insert(PathBuf::from("/tmp/bare_test/"), vec![
+                ("shooshoo.bar".to_string(),    "booboo.bar".to_string()),
+                ("foo-bar.qux".to_string(),     "foo.coconut.qux".to_string()),
+                ("_(grault).qux".to_string(),   ".grault.qux".to_string()),
+            ]);
+            m
+        });
     }
 
     #[cfg(unix)]
-    fn file_path_strings<'a>() -> Vec<&'a str> {
+    fn paths<'l>() -> Vec<&'l Path> {
         vec![
-            "/tmp/bare_test/foo.bar",
-            "/tmp/bare_test/foo-bar.qux",
-            "/tmp/bare_test/corge_(grault).quux",
+            Path::new("/tmp/bare_test/shooshoo.bar"),
+            Path::new("/tmp/bare_test/foo-bar.qux"),
+            Path::new("/tmp/bare_test/_(grault).qux"),
         ]
     }
 
     #[cfg(windows)]
-    fn file_path_strings<'a>() -> Vec<&'a str> {
+    fn paths<'l>() -> Vec<&'l Path> {
         vec![
             // TODO:
+        ]
+    }
+
+    fn patterns() -> Vec<(Regex, String)> {
+        vec![
+            (regex(r"shoo"),          replacement("boo")),
+            (regex(r"(-)bar"),        replacement("_coconut")),
+            (regex(r"\(grault\)"),    replacement("grault")),
+            (regex(r"_"),             replacement(".")),
         ]
     }
 
@@ -117,3 +121,5 @@ mod tests {
         }
     }
 }
+
+//  LocalWords:  PathBuf
